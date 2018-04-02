2eb0e581f58a3d0ba3bc4a714199e8a3a6fea903
namachan10777 <namachan10777@gmail.com>
2018-04-03 03:05:45 +0900

    impl depends parser

--- src/package_parser.d
+++ src/package_parser.d
@@ -4,8 +4,57 @@ import std.json;
 import std.algorithm.iteration : map;
 import std.range : array;
 import std.typecons : tuple;
+import std.format: format;
+import std.exception : collectExceptionMsg, assertThrown;
 
 import spam.config;
+
+bool is_white(in char c) {
+	return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+}
+Cond parseCond(string src) {
+	bool op_shifting;
+	string left, right;
+	size_t begin, idx;
+	while(begin < src.length && is_white(src[begin])) ++begin;
+	if (idx == src.length) throw new Exception("Syntax error at dependency version");
+	if (src[begin] == '>' || src[begin] == '<') {
+		if (begin + 1 < src.length && src[begin+1] == '='){
+			left = src[begin..begin+2];
+			begin = begin+2;
+		}
+		else {
+			left = src[begin..begin+1];
+			begin = begin+1;
+		}
+	}
+	else if (src[begin] == '=') {
+		left = src[begin..begin+1];
+		begin = begin+1;
+	}
+	while(idx < src.length && is_white(src[begin])) ++begin;
+	idx=begin;
+	while(idx < src.length && !is_white(src[idx])) ++idx;
+	right = src[begin..idx];
+	while(idx < src.length && is_white(src[idx])) ++idx;
+
+	if (idx != src.length || right.length == 0) throw new Exception("Syntax error at dependency version");
+	switch(left) {
+	case "<":  return Cond(CondType.Less,   right);
+	case "<=": return Cond(CondType.LessEq, right);
+	case ">":  return Cond(CondType.Gret,   right);
+	case ">=": return Cond(CondType.GretEq, right);
+	case "=":  return Cond(CondType.Eq,     right);
+	default:
+		throw new Exception("Syntax error at dependency version");
+	}
+}
+unittest {
+	assert(collectExceptionMsg(parseCond("")) == "Syntax error at dependency version");
+	assert(collectExceptionMsg(parseCond("= 1 a")) == "Syntax error at dependency version");
+	assert(collectExceptionMsg(parseCond("== 1 ")) == "Syntax error at dependency version");
+	assert(parseCond(" <= 1.4a") == Cond(CondType.LessEq, "1.4a"));
+}
 
 Config parseConfig (string str) {
 	auto json = parseJSON(str);
@@ -19,6 +68,23 @@ Config parseConfig (string str) {
 	if ("build"        in json.object) {
 		foreach (step; json.object["build"].array) {
 			config.build ~= step.array.map!(json => json.str).array;
+		}
+	}
+	if ("depends"      in json.object) {
+		foreach (dep_name; json.object["depends"].object.keys) {
+			Dep dep;
+			auto dep_json = json.object["depends"].object[dep_name];
+			if ("when" in dep_json.object) {
+				if (dep_json.object["when"].str == "build")
+					dep.type = DepType.Build;
+				else throw new Exception (format("Invalid \"when\" element's value %s", dep_json.object["when"].str));
+			}
+			else {
+				dep.type = DepType.Always;
+			}
+			auto cond_txt = dep_json.object["version"].str;
+			dep.cond = cond_txt.parseCond;
+			dep.name = dep_name;
 		}
 	}
 	//required field
